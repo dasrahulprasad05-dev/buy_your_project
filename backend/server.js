4e8efb03d2c06e6791d23f1387abd587ed496286
Rahul <dasrahulprasad05@gmail.com>
2026-07-14 10:34:37 +0530

    Update app.listen condition for Vercel

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== 'production') {
+if (!process.env.VERCEL) {
   app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
   });
